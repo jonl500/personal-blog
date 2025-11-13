e5fc0b06626b7fc5946bf91e17db4e4569e44e42
Jonathan <jonl500@gmail.com>
2025-11-12 23:43:15 -0500

    more scaffolding.

---
+++ personal-blog/src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Chaotic Zen'
+export const description = 'Personal blog of a software engineer/Human Being'
+export const url = dev ? 'http://localhost:5173/' : 'https://blog.innovevo.dev/'
--- personal-blog/src/routes/+layout.svelte
+++ personal-blog/src/routes/+layout.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
 	import '../app.css';
+	import Footer from '$lib/components/footer.svelte'
+	import Header from '$lib/components/header.svelte'
 	import favicon from '$lib/assets/favicon.svg';
 
-	let { children } = $props();
+	let { children, data } = $props();
 </script>
-
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+<div class="layout">
+	<Header />
+	<main>
+		{@render children?.()}
+	</main>
+	<Footer />
+</div>
 
-{@render children()}
+<style>
+	.layout {
+		height: 100%;
+		max-inline-size:  1440px; /*this is the max width of the page*/
+		display: grid; /*this is the layout of the page*/
+		grid-template-rows: auto 1fr auto; /*this is the height of the header, main, and footer*/
+		margin-inline: auto; /*this is the margin of the page*/
+		padding-inline: var(--size-7); /*this is the padding of the page*/
+	
+		@media (min-width: 1440px) {
+			padding-inline: 0;
+		}
+	}
+
+	main {
+		padding-block: var(--size-7);
+	}
+</style>
